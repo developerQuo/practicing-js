8bcb26271abc7c682142c3448896bc9674d77f25
changmin <developerQuo@gmail.com>
2024-04-03 21:21:00 +0900

    array filter-range-in-place test case 1 resolved

--- apps/array/filter-range-in-place.ts
+++ apps/array/filter-range-in-place.ts
@@ -1,4 +1,16 @@
 function filterRangeInPlace(arr: Array<number>, a: number, b: number){
+    const removeList: Array<number> = [];
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] < a || arr[i] > b) {
+            removeList.push(i)
+        }
+    }
+    console.log(removeList)
+    for (let i = arr.length - 1; i >= 0; i--){
+        if (removeList.includes(i)){
+            arr.splice(i, 1)
+        }
+    }
 }
 
 export default filterRangeInPlace;
