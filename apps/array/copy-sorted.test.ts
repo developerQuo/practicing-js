9551109484a162bae64d7b5d42ccab5011f0e868
changmin <developerQuo@gmail.com>
2024-04-04 15:05:08 +0900

    array copy-sorted ascending test

--- apps/array/copy-sorted.test.ts
+++ apps/array/copy-sorted.test.ts
@@ -1,9 +1,16 @@
-import { describe, it } from "@jest/globals";
+import { describe, expect, it } from "@jest/globals";
 import copySorted from "./copy-sorted";
 
 describe('배열 복사본을 정렬하기', () => {
     it('sorts array in ascending order', () => {
-        
+        let arr = ["React", "Java", "Dart", "Python"];
+        let sorted = copySorted(arr);
+        sorted.forEach((cur, i, array) => {
+            if (i) {
+                let result = cur >= array[i - 1]
+                expect(result).toBeTruthy()
+            }
+        })
     })
 
     it('must not be changed', () => {
