194bc063256565a2e2e8ab5a3fabf5441c267e0b
changmin <developerQuo@gmail.com>
2024-04-04 14:44:55 +0900

    fixed array sort-in-descending-order test case 1 logic

--- apps/array/sort-in-descending-order.test.ts
+++ apps/array/sort-in-descending-order.test.ts
@@ -2,13 +2,13 @@ import { describe, expect, it } from '@jest/globals';
 import sortDescendingOrder from './sort-in-descending-order';
 
 describe('내림차순으로 정렬하기', () => {
-    it('is greater than(or equal) left element', ()=> {
+    it('is less than(or equal) left element', ()=> {
         let arr = [1, 2, 0, -10, 8, -2];
         let sortedArr = sortDescendingOrder(arr);
         sortedArr.forEach((cur, i, array) => {
             if (i){
-            expect(cur).toBeGreaterThanOrEqual(array[i])
+            expect(cur).toBeLessThanOrEqual(array[i - 1])
             }
         })
     })
-})
+}) 
