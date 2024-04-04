d6f31f09739616096f0a1bf0067006f4043418e5
changmin <developerQuo@gmail.com>
2024-04-04 15:39:17 +0900

    array copy-sorted not-be-changed test

--- apps/array/copy-sorted.test.ts
+++ apps/array/copy-sorted.test.ts
@@ -1,9 +1,14 @@
-import { describe, expect, it } from "@jest/globals";
+import { beforeEach, describe, expect, it, test } from "@jest/globals";
 import copySorted from "./copy-sorted";
 
 describe('배열 복사본을 정렬하기', () => {
+    let arr: Array<string>;
+
+    beforeEach(() => {
+        arr = ["React", "Java", "Dart", "Python"];
+    })
+
     it('sorts array in ascending order', () => {
-        let arr = ["React", "Java", "Dart", "Python"];
         let sorted = copySorted(arr);
         sorted.forEach((cur, i, array) => {
             if (i) {
@@ -13,7 +18,11 @@ describe('배열 복사본을 정렬하기', () => {
         })
     })
 
-    it('must not be changed', () => {
-        
+    test('original array must not be changed', () => {
+        const snapshot = arr.slice()
+        copySorted(arr)
+        const isMatched = snapshot.every((cur, i) => cur === arr[i])
+        expect(isMatched).toBeTruthy()
+        expect(snapshot.length === arr.length).toBeTruthy()
     })
 })
