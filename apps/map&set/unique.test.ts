10fa06cc6ded0b26b90af49a89f5a84c1b2e5dd0
changmin <developerQuo@gmail.com>
2024-04-04 16:42:00 +0900

    set > unique's duplication test

--- apps/map&set/unique.test.ts
+++ apps/map&set/unique.test.ts
@@ -1,8 +1,28 @@
-import { describe, it } from '@jest/globals';
+import { beforeEach, describe, expect, it } from '@jest/globals';
 import unique from './unique';
 
 describe('배열에서 중복 요소 제거하기', () => {
-	it('deletes duplicate values', () => {});
+	it('deletes duplicate values', () => {
+		let values = [
+			'Hare',
+			'Krishna',
+			'Hare',
+			'Krishna',
+			'Krishna',
+			'Krishna',
+			'Hare',
+			'Hare',
+			':-O',
+		];
+
+		const hasUnique = unique(values).every((cur, i, array) => {
+			const snapshot = array.slice();
+			snapshot.splice(i, 1);
+			return !snapshot.includes(cur);
+		});
+
+		expect(hasUnique).toBeTruthy();
+	});
 
 	it("doesn't have data type constraints", () => {});
 });
