4b3afc2e11276be1453ad4f0173358e81a94350c
changmin <developerQuo@gmail.com>
2024-04-04 17:18:58 +0900

    set > anagram's test cases

---
+++ apps/map&set/anagram.test.ts
@@ -0,0 +1,13 @@
+import { beforeEach, describe, it } from '@jest/globals';
+
+describe('애너그램 걸러내기', () => {
+	let arr: Array<string>;
+
+	beforeEach(() => {
+		arr = ['nap', 'teachers', 'cheaters', 'PAN', 'ear', 'era', 'hectares'];
+	});
+
+	it('is pure function', () => {});
+
+	it('removes anagram words', () => {});
+});
---
+++ apps/map&set/anagram.ts
@@ -0,0 +1,3 @@
+export default function aclean(words: Array<string>) {
+	return words;
+}
