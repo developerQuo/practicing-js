a88addbd1826c59cab11a5a4bf52be6cd55c58a0
changmin <developerQuo@gmail.com>
2024-04-12 13:36:52 +0900

    weakSet > save-read-message's no-property test

--- apps/weakMap&weakSet/save-read-message.test.ts
+++ apps/weakMap&weakSet/save-read-message.test.ts
@@ -25,5 +25,22 @@ describe("'읽음'상태인 메시지 저장하기", () => {
 		// 가비지 컬렉션에 의해 제거가 되었는지는 브라우저 개발자 도구나 node.js 환경이 아니면 불가능.
 	});
 
-	test('messages array have not property', () => {});
+	test('messages array have not property', () => {
+		let propCheck: Array<boolean> = [];
+
+		for (let key in messages) {
+			propCheck.push(isNaN(+key));
+		}
+
+		expect(propCheck.some((check) => check)).toBeFalsy();
+
+		propCheck = [];
+
+		Object.assign(messages, { testProperty: 3 });
+		for (let key in messages) {
+			propCheck.push(isNaN(+key));
+		}
+		console.log(propCheck);
+		expect(propCheck.some((check) => check)).toBeTruthy();
+	});
 });
