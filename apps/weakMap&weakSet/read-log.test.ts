502f705639eade8b4053d74afe9565d4cffb68c5
changmin <developerQuo@gmail.com>
2024-04-12 14:15:33 +0900

    weakMap > read-logs's read-log test

--- apps/weakMap&weakSet/read-log.test.ts
+++ apps/weakMap&weakSet/read-log.test.ts
@@ -1,4 +1,5 @@
-import { beforeEach, describe, expect, it, test } from '@jest/globals';
+import { describe, expect, it } from '@jest/globals';
+import saveReadLog, { Message } from './read-log';
 
 let messages = [
 	{ text: 'Hello', from: 'John' },
@@ -8,6 +9,9 @@ let messages = [
 
 describe('읽은 날짜 저장하기', () => {
 	it('has read date logs', () => {
-		expect;
+		let readDate: WeakMap<Message, Date> = saveReadLog(messages[0]);
+
+		expect(readDate.has(messages[0])).toBeTruthy();
+		expect(typeof readDate.get(messages[0])).toBe('Date');
 	});
 });
--- apps/weakMap&weakSet/read-log.ts
+++ apps/weakMap&weakSet/read-log.ts
@@ -1,1 +1,5 @@
-export default function saveReadLog() {}
+export type Message = { text: string; from: string };
+
+export default function saveReadLog(message: Message): WeakMap<Message, Date> {
+	return new WeakMap();
+}
