cc03d97e5f2645e7c7ce3f7944724e1fd849687f
changmin <developerQuo@gmail.com>
2024-04-12 14:01:30 +0900

    weakMap > read-logs test cases

---
+++ apps/weakMap&weakSet/read-log.test.ts
@@ -0,0 +1,13 @@
+import { beforeEach, describe, expect, it, test } from '@jest/globals';
+
+let messages = [
+	{ text: 'Hello', from: 'John' },
+	{ text: 'How goes?', from: 'John' },
+	{ text: 'See you soon', from: 'Alice' },
+];
+
+describe('읽은 날짜 저장하기', () => {
+	it('has read date logs', () => {
+		expect;
+	});
+});
