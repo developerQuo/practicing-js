a272026adac507ccaf737073e8d98a1949e5673a
changmin <developerQuo@gmail.com>
2024-04-13 20:08:03 +0900

    add test case for count-properties

---
+++ apps/iteration/count-properties.test.ts
@@ -0,0 +1,8 @@
+import { describe, expect, it } from '@jest/globals';
+import count from './count-properties';
+
+describe('프로퍼티 값 더하기', () => {
+	it('count properties in object', () => {});
+
+	it('returns 0 for empty object', () => {});
+});
---
+++ apps/iteration/count-properties.ts
@@ -0,0 +1,4 @@
+export default function count(salaries: Record<string, number>): number {
+	let total = 0;
+	return total;
+}
