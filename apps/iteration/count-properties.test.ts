0b071c8b25bf13fa5916a12a379ca48f6d2daa14
changmin <developerQuo@gmail.com>
2024-04-13 20:21:08 +0900

    fix typo

--- apps/iteration/count-properties.test.ts
+++ apps/iteration/count-properties.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, it } from '@jest/globals';
 import count from './count-properties';
 
 describe('프로퍼티 값 더하기', () => {
-	it('count properties in object', () => {
+	it('counts the number of properties in object', () => {
 		expect(
 			count({
 				John: 100,
