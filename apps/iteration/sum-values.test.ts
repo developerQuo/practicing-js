e3615c61abacf43cb1f7da257bc7f5d9c8dd368d
changmin <developerQuo@gmail.com>
2024-04-13 19:36:49 +0900

    test code

--- apps/iteration/sum-values.test.ts
+++ apps/iteration/sum-values.test.ts
@@ -1,7 +1,26 @@
 import { beforeEach, describe, expect, it, test } from '@jest/globals';
+import sumSalaries from './sum-values';
 
 describe('프로퍼티 값 더하기', () => {
-	it('return sum of values in object', () => {});
+	it('return sum of values in object', () => {
+		expect(
+			sumSalaries({
+				John: 100,
+				Pete: 400,
+				Mary: 250,
+				Alice: 150,
+				Bob: 200,
+			})
+		).toBe(1100);
+
+		expect(
+			sumSalaries({
+				a: 1,
+				b: 2,
+				c: -3,
+			})
+		).toBe(-1);
+	});
 
 	it('returns 0 for empty object', () => {});
 });
