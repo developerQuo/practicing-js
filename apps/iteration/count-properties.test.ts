9b5a0bd6e64848c5d43a4974e27a565ff7e1edf2
changmin <developerQuo@gmail.com>
2024-04-13 20:11:33 +0900

    add logic for count-properties

--- apps/iteration/count-properties.test.ts
+++ apps/iteration/count-properties.test.ts
@@ -2,7 +2,25 @@ import { describe, expect, it } from '@jest/globals';
 import count from './count-properties';
 
 describe('프로퍼티 값 더하기', () => {
-	it('count properties in object', () => {});
+	it('count properties in object', () => {
+		expect(
+			count({
+				John: 100,
+				Pete: 400,
+				Mary: 250,
+				Alice: 150,
+				Bob: 200,
+			})
+		).toBe(5);
+
+		expect(
+			count({
+				a: 1,
+				b: 2,
+				c: -4,
+			})
+		).toBe(3);
+	});
 
 	it('returns 0 for empty object', () => {});
 });
--- apps/iteration/count-properties.ts
+++ apps/iteration/count-properties.ts
@@ -1,4 +1,3 @@
 export default function count(salaries: Record<string, number>): number {
-	let total = 0;
-	return total;
+	return Object.keys(salaries).length;
 }
