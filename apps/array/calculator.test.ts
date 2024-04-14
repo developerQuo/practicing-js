5314e004606fcece15f48249bc61c678a14688fd
changmin <developerQuo@gmail.com>
2024-04-14 22:34:47 +0900

    feature: Add unit tests for defensive code

--- apps/array/calculator.test.ts
+++ apps/array/calculator.test.ts
@@ -7,14 +7,18 @@ describe('확장 가능한 계산기', () => {
 
 		expect(calc.calculate('3 + 7')).toBe(10);
 		expect(calc.calculate('3 - 7')).toBe(-4);
+		expect(calc.calculate('31 + 71')).toBe(102);
+		expect(calc.calculate('3 - 7 - 1 + 4')).toBe(-1);
 
-		//TODO: test failures
+		//TODO: test failures - 방어 코드 짜기
 		// expect(calc.calculate('3  +  7')).toBe(10);
 		// expect(calc.calculate('3 + ')).toBe(3);
+		// expect(calc.calculate('33 32')).toBe(3332);
 		// expect(calc.calculate('3')).toBe(3);
 	});
 
 	it('extends function', () => {
 		let powerCalc = new Calculator();
+		//TODO: git stash pop
 	});
 });
