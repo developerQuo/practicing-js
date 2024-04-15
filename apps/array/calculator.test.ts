63455c7db4f650badeaa47545771a7f0c561df5a
changmin <developerQuo@gmail.com>
2024-04-15 17:47:20 +0900

    feature: add unit test for calculator

--- apps/array/calculator.test.ts
+++ apps/array/calculator.test.ts
@@ -9,12 +9,12 @@ describe('확장 가능한 계산기', () => {
 		expect(calc.calculate('3 - 7')).toBe(-4);
 		expect(calc.calculate('31 + 71')).toBe(102);
 		expect(calc.calculate('3 - 7 - 1 + 4')).toBe(-1);
+		expect(calc.calculate('3')).toBe(3);
 
-		//TODO: test failures - 방어 코드 짜기
+		// must failing test
 		// expect(calc.calculate('3  +  7')).toBe(10);
 		// expect(calc.calculate('3 + ')).toBe(3);
 		// expect(calc.calculate('33 32')).toBe(3332);
-		// expect(calc.calculate('3')).toBe(3);
 	});
 
 	it('extends function', () => {
@@ -27,7 +27,7 @@ describe('확장 가능한 계산기', () => {
 		expect(powerCalc.calculate('2 ** 3')).toBe(8);
 		expect(powerCalc.calculate('6 / 3')).toBe(2);
 
-		//TODO: test failures
+		// must failing test
 		// powerCalc.addMethod('', (a, b) => a + b);
 		// powerCalc.addMethod('*', (a, b) => a + b);
 	});
