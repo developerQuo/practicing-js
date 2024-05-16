4aac8df95df5e49550bdbeb91b5c3641e72c2abe
changmin <developerQuo@gmail.com>
2024-05-16 22:09:29 +0900

    feat: add unit test for europe calendar

--- apps/date/europe-calendar.test.ts
+++ apps/date/europe-calendar.test.ts
@@ -2,5 +2,11 @@ import { describe, expect, it } from '@jest/globals';
 import getLocalDay from './europe-calendar';
 
 describe('유럽 기준 달력', () => {
-	it('return week day index from date', () => {});
+	it('return week day index from date', () => {
+		let May16_2024 = new Date(2024, 5 - 1, 16);
+		expect(getLocalDay(May16_2024)).toBe(4);
+
+		let May1_2024 = new Date(2024, 5 - 1, 11);
+		expect(getLocalDay(May1_2024)).toBe(6);
+	});
 });
