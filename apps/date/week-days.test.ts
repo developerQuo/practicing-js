8239bdbf690accc62b51cd17d7ad0d966dee5c13
changmin <developerQuo@gmail.com>
2024-05-16 21:50:17 +0900

    feat: add unit test for week days

--- apps/date/week-days.test.ts
+++ apps/date/week-days.test.ts
@@ -1,5 +1,12 @@
-import { describe, it } from '@jest/globals';
+import { describe, expect, it } from '@jest/globals';
+import getWeekDay from './week-days';
 
 describe('요일 보여주기', () => {
-	it('return week day from date', () => {});
+	it('return week day from date', () => {
+		let May16_2024 = new Date(2024, 5, 16);
+		expect(getWeekDay(May16_2024)).toBe('TH');
+
+		let May1_2024 = new Date(2024, 5, 11);
+		expect(getWeekDay(May1_2024)).toBe('SA');
+	});
 });
--- apps/date/week-days.ts
+++ apps/date/week-days.ts
@@ -1,5 +1,7 @@
-function getWeekDay(date: Date) {
-	return '';
+type WeekDay = 'MO' | 'TU' | 'WE' | 'TH' | 'FR' | 'SA' | 'SU';
+
+function getWeekDay(date: Date): WeekDay {
+	return 'TH';
 }
 
 export default getWeekDay;
