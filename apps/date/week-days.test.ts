34e4fb899893ece2604748a37b7a8a389d94a6dd
changmin <developerQuo@gmail.com>
2024-05-16 21:59:26 +0900

    feat: add logic for week days

--- apps/date/week-days.test.ts
+++ apps/date/week-days.test.ts
@@ -3,10 +3,10 @@ import getWeekDay from './week-days';
 
 describe('요일 보여주기', () => {
 	it('return week day from date', () => {
-		let May16_2024 = new Date(2024, 5, 16);
+		let May16_2024 = new Date(2024, 5 - 1, 16);
 		expect(getWeekDay(May16_2024)).toBe('TH');
 
-		let May1_2024 = new Date(2024, 5, 11);
+		let May1_2024 = new Date(2024, 5 - 1, 11);
 		expect(getWeekDay(May1_2024)).toBe('SA');
 	});
 });
--- apps/date/week-days.ts
+++ apps/date/week-days.ts
@@ -1,7 +1,15 @@
-type WeekDay = 'MO' | 'TU' | 'WE' | 'TH' | 'FR' | 'SA' | 'SU';
+const weekDayMap = {
+	0: 'SU',
+	1: 'MO',
+	2: 'TU',
+	3: 'WE',
+	4: 'TH',
+	5: 'FR',
+	6: 'SA',
+};
 
-function getWeekDay(date: Date): WeekDay {
-	return 'TH';
+function getWeekDay(date: Date) {
+	return weekDayMap[date.getDay()];
 }
 
 export default getWeekDay;
