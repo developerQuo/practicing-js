a4196a36a4492c819752fa3b869565c4a8eeb9a0
changmin <developerQuo@gmail.com>
2024-05-17 20:40:37 +0900

    feat: add unit test interface for serialization

---
+++ apps/json/serialization.test.ts
@@ -0,0 +1,12 @@
+import { describe, expect, it } from '@jest/globals';
+
+let user = {
+	name: 'John Smith',
+	age: 35,
+};
+
+describe('객체를 JSON으로 바꾼 후 다시 객체로 바꾸기', () => {
+	it('serializes an object to JSON', () => {});
+
+	it('deserializes a JSON to object', () => {});
+});
