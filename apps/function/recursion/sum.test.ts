8789cffed75ba1579007db8d513593102e05d999
changmin <developerQuo@gmail.com>
2024-05-18 19:14:03 +0900

    feat: add unit test for recursion's sum recursion

--- apps/function/recursion/sum.test.ts
+++ apps/function/recursion/sum.test.ts
@@ -1,14 +1,20 @@
 import { describe, expect, test } from '@jest/globals';
-import { sumForLoop } from './sum';
+import { sumForLoop, sumRecursion } from './sum';
 
 describe('주어진 숫자까지의 모든 숫자 더하기', () => {
 	describe('it sums numbers', () => {
 		test('for loop', () => {
-			expect(sumForLoop(5)).toBe(15);
-			expect(sumForLoop(1)).toBe(1);
-			expect(sumForLoop(10_000)).toBe(50005000);
+			testSumTo(sumForLoop);
 		});
-		test('recursion', () => {});
+		test('recursion', () => {
+			testSumTo(sumRecursion);
+		});
 		test('등차수열 공식', () => {});
 	});
 });
+
+function testSumTo(calculator: (num: number) => number) {
+	expect(calculator(5)).toBe(15);
+	expect(calculator(1)).toBe(1);
+	expect(calculator(10_000)).toBe(50005000);
+}
