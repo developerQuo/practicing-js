6cd551b0c484adbe16d4a5988117d002ac1da95b
changmin <developerQuo@gmail.com>
2024-05-18 18:46:10 +0900

    feat: add unit test interface for recursion's sum

---
+++ apps/function/recursion/sum.test.ts
@@ -0,0 +1,9 @@
+import { describe, expect, test } from '@jest/globals';
+
+describe('주어진 숫자까지의 모든 숫자 더하기', () => {
+	describe('it sums numbers', () => {
+		test('for loop', () => {});
+		test('recursion', () => {});
+		test('등차수열 공식', () => {});
+	});
+});
---
+++ apps/function/recursion/sum.ts
@@ -0,0 +1,16 @@
+/** 세 가지 답안 */
+
+// 1. for loop
+function sumForLoop(num: number) {
+	return 0;
+}
+
+// 2. recursion
+function sumRecursion(num: number) {
+	return 0;
+}
+
+// 3. 등차수열
+function sumAP(num: number) {
+	return 0;
+}
