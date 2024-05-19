5853144a2d92f679ffed2ed82305d28c5b0fe5a0
changmin <developerQuo@gmail.com>
2024-05-19 19:41:33 +0900

    feat: add unit test for recursion's factorial

--- apps/function/recursion/factorial.test.ts
+++ apps/function/recursion/factorial.test.ts
@@ -2,5 +2,11 @@ import { describe, expect, it } from '@jest/globals';
 import { factorial } from './factorial';
 
 describe('팩토리얼 계산하기', () => {
-	it('calculates the product of all numbers from 1 to n', () => {});
+	it('calculates the product of all numbers from 1 to n', () => {
+		expect(factorial(1)).toBe(1);
+		expect(factorial(2)).toBe(2);
+		expect(factorial(3)).toBe(6);
+		expect(factorial(4)).toBe(24);
+		expect(factorial(5)).toBe(120);
+	});
 });
