09d5e322c2a01543bf50176350ffc198ab732a4f
changmin <developerQuo@gmail.com>
2024-05-19 19:52:45 +0900

    feat: add unit test for recursion's fibonacci calc

--- apps/function/recursion/fibonacci.test.ts
+++ apps/function/recursion/fibonacci.test.ts
@@ -2,7 +2,11 @@ import { describe, expect, it } from '@jest/globals';
 import { fib } from './fibonacci';
 
 describe('피보나치 수 계산하기', () => {
-	it('returns the correct fibonacci number for a given input', () => {});
+	it('returns the correct fibonacci number for a given input', () => {
+		expect(fib(3)).toBe(2);
+		expect(fib(7)).toBe(13);
+		expect(fib(77)).toBe(5527939700884757);
+	});
 
 	it('completes within 1 second', () => {});
 });
