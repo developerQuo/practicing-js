921ee022afdfb7dca21fcbda13cf074d775b3e66
changmin <developerQuo@gmail.com>
2024-05-19 19:51:24 +0900

    feat: add unit test interface for recursion's fibonacci

---
+++ apps/function/recursion/fibonacci.test.ts
@@ -0,0 +1,8 @@
+import { describe, expect, it } from '@jest/globals';
+import { fib } from './fibonacci';
+
+describe('피보나치 수 계산하기', () => {
+	it('returns the correct fibonacci number for a given input', () => {});
+
+	it('completes within 1 second', () => {});
+});
---
+++ apps/function/recursion/fibonacci.ts
@@ -0,0 +1,3 @@
+export function fib(num: number) {
+	return 0;
+}
