0745a616b6ef66c269c2d7511ff580e7b09ce9bd
changmin <developerQuo@gmail.com>
2024-05-20 18:55:31 +0900

    test: add unit test for single-linked list

--- apps/function/recursion/single-linked-list.test.ts
+++ apps/function/recursion/single-linked-list.test.ts
@@ -1,5 +1,11 @@
-import { describe, expect, it } from '@jest/globals';
-import { List } from './single-linked-list';
+import { describe, expect, it, jest } from '@jest/globals';
+import {
+	List,
+	printListForLoop,
+	printListRecursion,
+} from './single-linked-list';
+
+jest.mock('console');
 
 let list: List = {
 	value: 1,
@@ -16,5 +22,23 @@ let list: List = {
 };
 
 describe('단일 연결 리스트 출력하기', () => {
-	it('prints values one by one in order', () => {});
+	it('prints values one by one in order (for loop)', () => {
+		printListForLoop(list);
+
+		expect(console.log).toHaveBeenCalledTimes(4);
+		expect(console.log).toHaveBeenCalledWith(1);
+		expect(console.log).toHaveBeenCalledWith(2);
+		expect(console.log).toHaveBeenCalledWith(3);
+		expect(console.log).toHaveBeenCalledWith(4);
+	});
+
+	it('prints values one by one in order (recursion)', () => {
+		printListRecursion(list);
+
+		expect(console.log).toHaveBeenCalledTimes(4);
+		expect(console.log).toHaveBeenCalledWith(1);
+		expect(console.log).toHaveBeenCalledWith(2);
+		expect(console.log).toHaveBeenCalledWith(3);
+		expect(console.log).toHaveBeenCalledWith(4);
+	});
 });
