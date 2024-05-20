62da91250feaed97f36b1b983e7019940e5d9b62
changmin <developerQuo@gmail.com>
2024-05-20 19:08:40 +0900

    feat: add print-list-for-loop for single-linked list

--- apps/function/recursion/single-linked-list.ts
+++ apps/function/recursion/single-linked-list.ts
@@ -4,7 +4,16 @@ export type List = {
 };
 
 // for loop
-export function printListForLoop(list: List) {}
+export function printListForLoop(list: List) {
+	while (true) {
+		console.log(list.value);
+		if (list.next) {
+			list = list.next;
+		} else {
+			break;
+		}
+	}
+}
 
 // recursion
 export function printListRecursion(list: List) {}
