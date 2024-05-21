c893eccb7574ad045311834655bb98b97b57cb57
changmin <developerQuo@gmail.com>
2024-05-21 21:47:55 +0900

    test: ensure sum function adds two numbers

--- apps/closure/sum.test.ts
+++ apps/closure/sum.test.ts
@@ -1,7 +1,11 @@
 import { describe, expect, it } from '@jest/globals';
+import sum from './sum';
 
 describe('클로저를 이용하여 합 구하기', () => {
-	it('sums two numbers', () => {});
+	it('sums two numbers', () => {
+		expect(sum(1)(2)).toBe(3);
+		expect(sum(5)(-1)).toBe(4);
+	});
 
 	it('rejects non numbers', () => {});
 
