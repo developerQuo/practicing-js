1f4d360fb9142d7f420c4a699612762bb8c9370f
changmin <developerQuo@gmail.com>
2024-05-21 22:36:01 +0900

    test: add sum function that rejects non number

--- apps/closure/sum.ts
+++ apps/closure/sum.ts
@@ -1,5 +1,11 @@
 export default function sum(prevNum: number) {
 	return function (nextNum: number) {
-		return prevNum + nextNum;
+		if (isNumber(nextNum) && isNumber(prevNum)) {
+			return prevNum + nextNum;
+		}
+		throw new Error("There isn't number");
 	};
 }
+
+const isNumber = (num: number) =>
+	typeof num == 'number' && !isNaN(num) && isFinite(num);
