4b9ac4ace39e73800c3dcb62e013a94a22ffc8aa
changmin <developerQuo@gmail.com>
2024-05-21 21:53:37 +0900

    test: ensure sum function rejects non number

--- apps/closure/sum.test.ts
+++ apps/closure/sum.test.ts
@@ -7,7 +7,10 @@ describe('클로저를 이용하여 합 구하기', () => {
 		expect(sum(5)(-1)).toBe(4);
 	});
 
-	it('rejects non numbers', () => {});
+	it('rejects non numbers', () => {
+		expect(sum(NaN)(2)).toThrowError('There is non number');
+		expect(sum(5)(1 / 0)).toThrowError('There is non number');
+	});
 
 	it('uses a closure to remember state', () => {});
 });
