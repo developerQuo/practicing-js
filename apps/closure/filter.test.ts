bcb2c7f4c09dada44875ec88c1af6d14cf1711d5
changmin <developerQuo@gmail.com>
2024-05-22 20:10:53 +0900

    test: add in-array for closure filter

--- apps/closure/filter.test.ts
+++ apps/closure/filter.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from '@jest/globals';
-import { isBetween } from './filter';
+import { inArray, isBetween } from './filter';
 
 describe('함수를 이용해 원하는 값만 걸러내기', () => {
 	describe('in between function', () => {
@@ -17,6 +17,16 @@ describe('함수를 이용해 원하는 값만 걸러내기', () => {
 	});
 
 	describe('in array function', () => {
-		it('returns numbers matching in array', () => {});
+		it('returns numbers matching in array', () => {
+			let arr = [1, 2, 3, 4, 5, 6, 7];
+
+			let numbers1_2_10 = arr.filter(inArray([1, 2, 10]));
+
+			expect(numbers1_2_10).toEqual([1, 2]);
+
+			let emptyList = arr.filter(inArray([]));
+
+			expect(emptyList).toEqual([]);
+		});
 	});
 });
