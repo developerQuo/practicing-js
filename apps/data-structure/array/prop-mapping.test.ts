b5e1d1c3d251b66362e64e0ab12bd408ae0a4005
changmin <developerQuo@gmail.com>
2024-05-23 09:25:12 +0900

    test: add unit test of prop-mapping

--- apps/data-structure/array/prop-mapping.test.ts
+++ apps/data-structure/array/prop-mapping.test.ts
@@ -1,5 +1,14 @@
 import { describe, expect, it, test } from '@jest/globals';
+import { makeNameList } from './prop-mapping';
+
+let john = { name: 'John', age: 25 };
+let pete = { name: 'Pete', age: 30 };
+let mary = { name: 'Mary', age: 28 };
+
+let users = [john, pete, mary];
 
 describe('이름 매핑하기', () => {
-	it('returns name property list', () => {});
+	it('returns name property list', () => {
+		expect(makeNameList(users)).toEqual(['John', 'Pete', 'Mary']);
+	});
 });
