c7a406feace7168d62bbd4895afd70da0131f7bd
changmin <developerQuo@gmail.com>
2024-05-25 10:37:01 +0900

    feat: add NFE function increases 1

--- apps/function/named-function-expression/counter.ts
+++ apps/function/named-function-expression/counter.ts
@@ -1,5 +1,13 @@
 export function makeNFECounter() {
 	let count = 0;
+
+	function counter() {
+		return counter.count++;
+	}
+
+	counter.count = count;
+
+	return counter;
 }
 
 export function makeClosureCounter() {}
