e7f6df2d944ec3662393ded036153b5170d42f9f
changmin <developerQuo@gmail.com>
2024-05-25 11:14:08 +0900

    feat: add NFE function decreases 1

--- apps/function/named-function-expression/counter.ts
+++ apps/function/named-function-expression/counter.ts
@@ -8,6 +8,7 @@ export function makeNFECounter() {
 	// TODO: 왜 counter 함수 내부에서는 초기화 할 수 없지?
 	counter.count = count;
 	counter.set = (num: number) => (counter.count = num);
+	counter.decrease = () => counter.count--;
 
 	return counter;
 }
