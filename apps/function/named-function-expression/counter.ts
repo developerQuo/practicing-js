f68f1a931dc374e89b41c281854f0140750dda78
changmin <developerQuo@gmail.com>
2024-05-25 10:53:36 +0900

    feat: add NFE function sets number

--- apps/function/named-function-expression/counter.ts
+++ apps/function/named-function-expression/counter.ts
@@ -5,7 +5,9 @@ export function makeNFECounter() {
 		return counter.count++;
 	}
 
+	// TODO: 왜 counter 함수 내부에서는 초기화 할 수 없지?
 	counter.count = count;
+	counter.set = (num: number) => (counter.count = num);
 
 	return counter;
 }
