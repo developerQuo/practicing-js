4f3ec6d8f67151e9c7cdb60af8a33f4543c353aa
changmin <developerQuo@gmail.com>
2024-05-25 10:23:25 +0900

    test: add interface for NFE counter

---
+++ apps/function/named-function-expression/counter.test.ts
@@ -0,0 +1,19 @@
+import { beforeEach, describe, expect, it, test } from '@jest/globals';
+
+describe('숫자 설정과 감소가 가능한 counter 만들기', () => {
+	describe('named-function-expression', () => {
+		it('returns number if counter function called', () => {});
+
+		it('sets value by calling set property', () => {});
+
+		it('decrease counter 1 by calling decrease property', () => {});
+	});
+
+	describe('closure', () => {
+		it('returns number if counter function called', () => {});
+
+		it('sets value by calling set property', () => {});
+
+		it('decrease counter 1 by calling decrease property', () => {});
+	});
+});
---
+++ apps/function/named-function-expression/counter.ts
@@ -0,0 +1,3 @@
+export function makeNFECounter() {}
+
+export function makeClosureCounter() {}
