a09f238fb1ffc15ea958676636fc12178fe3b018
changmin <developerQuo@gmail.com>
2024-05-25 11:54:52 +0900

    test: add NEF function sums with positive number

--- apps/function/named-function-expression/sum.test.ts
+++ apps/function/named-function-expression/sum.test.ts
@@ -2,7 +2,10 @@ import { describe, expect, test } from '@jest/globals';
 
 describe('임의의 수만큼 있는 괄호를 이용해 합계 구하기', () => {
 	describe('sums all of numbers that chained parameter', () => {
-		test('positive', () => {});
+		test('positive', () => {
+			expect(sum(1)(2)).toBe(3);
+			expect(sum(1)(2)(3)).toBe(6);
+		});
 
 		test('negative', () => {});
 
