becbfe3b97ccc875f06d7b6bd78a00296882d8f5
changmin <developerQuo@gmail.com>
2024-05-26 10:13:15 +0900

    feat: add scheduling print-number interval test

--- apps/function/scheduling/print-number.ts
+++ apps/function/scheduling/print-number.ts
@@ -1,5 +1,14 @@
 // setInterval
-export function setIntervalPrintNumbers(from: number, to: number) {}
+export function setIntervalPrintNumbers(from: number, to: number) {
+	let current = from;
+
+	let timerId = setInterval(() => {
+		console.log(current);
+
+		if (current === to) clearInterval(timerId);
+		current++;
+	}, 1000);
+}
 
 // setTimer
 export function setTimerPrintNumbers(from: number, to: number) {}
