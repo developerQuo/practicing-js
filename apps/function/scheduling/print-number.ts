bcad95d14a34ef0d47feda2a5bb117f50bc26c3d
changmin <developerQuo@gmail.com>
2024-05-26 10:24:26 +0900

    feat: add scheduling print-number timer test

--- apps/function/scheduling/print-number.ts
+++ apps/function/scheduling/print-number.ts
@@ -11,4 +11,21 @@ export function setIntervalPrintNumbers(from: number, to: number) {
 }
 
 // setTimer
-export function setTimerPrintNumbers(from: number, to: number) {}
+export function setTimerPrintNumbers(from: number, to: number) {
+	let current = from;
+
+	const timerId = setTimeout(function timer() {
+		console.log(current);
+
+		if (current !== to) {
+			current++;
+			setTimeout(timer, 1000);
+		}
+	}, 1000);
+
+	return () => {
+		if (timerId) {
+			clearTimeout(timerId);
+		}
+	};
+}
