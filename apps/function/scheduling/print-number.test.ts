cd4f881d8748893d67e0c5866c07183db7b26479
changmin <developerQuo@gmail.com>
2024-05-26 10:13:04 +0900

    test: fix scheduling print-number interval test

--- apps/function/scheduling/print-number.test.ts
+++ apps/function/scheduling/print-number.test.ts
@@ -13,8 +13,10 @@ describe('일 초 간격으로 숫자 출력하기', () => {
 	it('prints numbers at one-second intervals (setInterval)', () => {
 		setIntervalPrintNumbers(2, 4);
 
+		jest.advanceTimersByTime(4000);
+
 		expect(setInterval).toHaveBeenCalledWith(expect.any(Function), 1000);
-		expect(setInterval).toHaveBeenCalledTimes(3);
+		expect(setInterval).toHaveBeenCalledTimes(1);
 
 		expect(console.log).toHaveBeenCalledWith(2);
 		expect(console.log).toHaveBeenCalledWith(3);
