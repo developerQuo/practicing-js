c0893bda354acffa9fa65e076867c539cd36583a
changmin <developerQuo@gmail.com>
2024-05-26 09:46:31 +0900

    test: add scheduling print-number interval test

--- apps/function/scheduling/print-number.test.ts
+++ apps/function/scheduling/print-number.test.ts
@@ -1,7 +1,25 @@
-import { describe, expect, it } from '@jest/globals';
+import { beforeEach, describe, expect, it, jest } from '@jest/globals';
+import { setIntervalPrintNumbers } from './print-number';
+
+jest.useFakeTimers();
+jest.spyOn(global, 'setInterval');
+jest.spyOn(global, 'setTimeout');
 
 describe('일 초 간격으로 숫자 출력하기', () => {
-	it('prints numbers at one-second intervals (setInterval)', () => {});
+	beforeEach(() => {
+		console.log = jest.fn();
+	});
+
+	it('prints numbers at one-second intervals (setInterval)', () => {
+		setIntervalPrintNumbers(2, 4);
+
+		expect(setInterval).toHaveBeenCalledWith(expect.any(Function), 1000);
+		expect(setInterval).toHaveBeenCalledTimes(3);
+
+		expect(console.log).toHaveBeenCalledWith(2);
+		expect(console.log).toHaveBeenCalledWith(3);
+		expect(console.log).toHaveBeenCalledWith(4);
+	});
 
 	it('prints numbers at one-second intervals (setTimer)', () => {});
 });
