fc4304f7ab731a68d1e985419e792e7acb40193c
changmin <developerQuo@gmail.com>
2024-05-27 12:44:40 +0900

    test: fix execute delay decorator after for ms

--- apps/function/decorator/delay.test.ts
+++ apps/function/decorator/delay.test.ts
@@ -3,6 +3,7 @@ import { delay } from './delay';
 
 jest.useFakeTimers();
 jest.spyOn(global, 'setTimeout');
+console.log = jest.fn();
 /**
  * f 함수 실행을 ms만큼 딜레이시킨다.
  * f 함수는 n개의 인수를 받을 수 있어야 하고, this 컨텍스트를 전달받아야 한다.
@@ -16,23 +17,30 @@ describe('Delaying decorator', () => {
 		};
 	});
 
-	it('executes function after for ms', () => {
-		console.log = jest.fn();
-
+	it('executes function after for 1000ms', () => {
 		let f1000 = delay(f, 1000);
+
 		f1000('f1000');
-		let f1500 = delay(f, 1500);
-		f1500('f1500');
 
 		expect(setTimeout).toHaveBeenCalledTimes(1);
 		expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 1000);
-		expect(console.log).toHaveBeenCalledTimes(1);
-		expect(console.log).toHaveBeenCalledWith('f1000');
 
 		jest.runOnlyPendingTimers();
 
+		expect(console.log).toHaveBeenCalledTimes(1);
+		expect(console.log).toHaveBeenCalledWith('f1000');
+	});
+
+	it('executes function after for 1500ms', () => {
+		let f1500 = delay(f, 1500);
+
+		f1500('f1500');
+
 		expect(setTimeout).toHaveBeenCalledTimes(2);
 		expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 1500);
+
+		jest.runOnlyPendingTimers();
+
 		expect(console.log).toHaveBeenCalledTimes(2);
 		expect(console.log).toHaveBeenCalledWith('f1500');
 	});
