4bb5e7115344b97ca823141af2067ac9104fe02b
changmin <developerQuo@gmail.com>
2024-05-27 11:26:01 +0900

    test: add execute delay decorator after for ms

--- apps/function/decorator/delay.test.ts
+++ apps/function/decorator/delay.test.ts
@@ -1,5 +1,8 @@
 import { beforeEach, describe, expect, it, jest } from '@jest/globals';
+import { delay } from './delay';
 
+jest.useFakeTimers();
+jest.spyOn(global, 'setTimeout');
 /**
  * f 함수 실행을 ms만큼 딜레이시킨다.
  * f 함수는 n개의 인수를 받을 수 있어야 하고, this 컨텍스트를 전달받아야 한다.
@@ -13,5 +16,24 @@ describe('Delaying decorator', () => {
 		};
 	});
 
-	it('executes function after for ms', () => {});
+	it('executes function after for ms', () => {
+		console.log = jest.fn();
+
+		let f1000 = delay(f, 1000);
+		f1000('f1000');
+		let f1500 = delay(f, 1500);
+		f1500('f1500');
+
+		expect(setTimeout).toHaveBeenCalledTimes(1);
+		expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 1000);
+		expect(console.log).toHaveBeenCalledTimes(1);
+		expect(console.log).toHaveBeenCalledWith('f1000');
+
+		jest.runOnlyPendingTimers();
+
+		expect(setTimeout).toHaveBeenCalledTimes(2);
+		expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 1500);
+		expect(console.log).toHaveBeenCalledTimes(2);
+		expect(console.log).toHaveBeenCalledWith('f1500');
+	});
 });
