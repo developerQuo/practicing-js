1e46d05f5a876b3c48e8f962de185f5e24423629
changmin <developerQuo@gmail.com>
2024-05-27 12:48:11 +0900

    feat: fix execute delay decorator after for ms

--- apps/function/decorator/delay.ts
+++ apps/function/decorator/delay.ts
@@ -1,3 +1,12 @@
-export const delay = function (f: Function, ms) {
-	return function () {};
+export const delay = function (f: Function, ms: number) {
+	return function (_) {
+		const timerId = setTimeout(() => {
+			// arrow function을 써서 this와 arguments는 wrapper에서 온다.
+			f.apply(this, arguments);
+		}, ms);
+
+		return () => {
+			clearTimeout(timerId);
+		};
+	};
 };
