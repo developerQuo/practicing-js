eea09c432a43ee93b514a6635d9a5af4ac294ca3
changmin <developerQuo@gmail.com>
2024-05-27 10:56:53 +0900

    feat: add counting calls that spy decorator

--- apps/function/decorator/spy.test.ts
+++ apps/function/decorator/spy.test.ts
@@ -1,12 +1,13 @@
 import { describe, expect, it, jest } from '@jest/globals';
-import { spy } from './spy';
+import { Spy, spy } from './spy';
 
 // 프로퍼티로 호출 횟수 저장.
 // 저장 형태는 [arg1, arg2][]
 describe('Spy decorator', () => {
 	it('counts the number of function call', () => {
-		let work = function (a: number, b: number) {
-			alert(a + b); // work is an arbitrary function or method
+		let work: Spy = function (a: number, b: number) {
+			console.log(a + b);
+			return a + b;
 		};
 
 		work = spy(work);
@@ -14,6 +15,7 @@ describe('Spy decorator', () => {
 		work(1, 2); // 3
 		work(4, 5); // 9
 
+		console.log(work.calls);
 		expect(work.calls).toEqual([
 			[1, 2],
 			[4, 5],
--- apps/function/decorator/spy.ts
+++ apps/function/decorator/spy.ts
@@ -1,4 +1,17 @@
-// setInterval
-export function spy(func: Function) {
-	return function () {};
-}
+export type Spy = {
+	(a: number, b: number): number;
+	calls?: [number, number][];
+};
+
+export const spy = function (func: Function) {
+	function wrapper() {
+		// 실행 횟수 넣기
+		wrapper.calls.push([...arguments]);
+		// 함수 실행
+		return func.apply(this, arguments);
+	}
+
+	wrapper.calls = [];
+
+	return wrapper;
+};
