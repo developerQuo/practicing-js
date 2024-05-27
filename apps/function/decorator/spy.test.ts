453ea9511c0702795da5e6318a63b5e9e8dbd412
changmin <developerQuo@gmail.com>
2024-05-27 10:23:07 +0900

    test: add counting calls that spy decorator

--- apps/function/decorator/spy.test.ts
+++ apps/function/decorator/spy.test.ts
@@ -1,13 +1,24 @@
 import { describe, expect, it, jest } from '@jest/globals';
-
-function work(a, b) {
-	alert(a + b); // work is an arbitrary function or method
-}
+import { spy } from './spy';
 
 // 프로퍼티로 호출 횟수 저장.
 // 저장 형태는 [arg1, arg2][]
 describe('Spy decorator', () => {
-	it('counts the number of function call', () => {});
+	it('counts the number of function call', () => {
+		let work = function (a: number, b: number) {
+			alert(a + b); // work is an arbitrary function or method
+		};
+
+		work = spy(work);
+
+		work(1, 2); // 3
+		work(4, 5); // 9
+
+		expect(work.calls).toEqual([
+			[1, 2],
+			[4, 5],
+		]);
+	});
 
 	it('saves every call as an array of arguments', () => {});
 });
