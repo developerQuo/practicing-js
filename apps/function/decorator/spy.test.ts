d425ab8e680c4ffc9e76435cfc5ef7f858b2bbaa
changmin <developerQuo@gmail.com>
2024-05-27 11:02:03 +0900

    test: add return an array on spy decorator

--- apps/function/decorator/spy.test.ts
+++ apps/function/decorator/spy.test.ts
@@ -1,26 +1,37 @@
-import { describe, expect, it, jest } from '@jest/globals';
+import { beforeEach, describe, expect, it, jest } from '@jest/globals';
 import { Spy, spy } from './spy';
 
 // 프로퍼티로 호출 횟수 저장.
 // 저장 형태는 [arg1, arg2][]
 describe('Spy decorator', () => {
-	it('counts the number of function call', () => {
-		let work: Spy = function (a: number, b: number) {
+	let work: Spy;
+
+	beforeEach(() => {
+		work = function (a: number, b: number) {
 			console.log(a + b);
 			return a + b;
 		};
+	});
 
+	it('counts the number of function call', () => {
 		work = spy(work);
 
 		work(1, 2); // 3
 		work(4, 5); // 9
 
-		console.log(work.calls);
 		expect(work.calls).toEqual([
 			[1, 2],
 			[4, 5],
 		]);
 	});
 
-	it('saves every call as an array of arguments', () => {});
+	it('saves every call as an array of arguments', () => {
+		work = spy(work);
+
+		work(1, 2); // 3
+		work(4, 5); // 9
+
+		expect(Array.isArray(work.calls)).toBeTruthy();
+		expect(Array.isArray(work.calls![0])).toBeTruthy();
+	});
 });
