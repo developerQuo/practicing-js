9365f8ef2c1a0238945b651ce96d074a47cd6d9c
changmin <developerQuo@gmail.com>
2024-06-04 08:08:58 +0900

    test: add object mapping interface

---
+++ apps/data-structure/array/obj-mapping.test.ts
@@ -0,0 +1,12 @@
+import { describe, expect, it } from '@jest/globals';
+import { userMapper } from './obj-mapping';
+
+let john = { name: 'John', surname: 'Smith', id: 1 };
+let pete = { name: 'Pete', surname: 'Hunt', id: 2 };
+let mary = { name: 'Mary', surname: 'Key', id: 3 };
+
+let users = [john, pete, mary];
+
+describe('객체 매핑하기', () => {
+	it('returns objects that have fullName property', () => {});
+});
