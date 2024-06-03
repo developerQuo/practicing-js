83f693d9e66d986f1d273bbf1a1dae0bd2e4c8dc
changmin <developerQuo@gmail.com>
2024-06-04 08:25:11 +0900

    test: add average age interface

---
+++ apps/data-structure/array/average-age.test.ts
@@ -0,0 +1,12 @@
+import { describe, expect, it } from '@jest/globals';
+import { getAverageAge } from './average-age';
+
+let john = { name: 'John', age: 25 };
+let pete = { name: 'Pete', age: 30 };
+let mary = { name: 'Mary', age: 29 };
+
+let arr = [john, pete, mary];
+
+describe('평균 나이 구하기', () => {
+	it('returns average age', () => {});
+});
