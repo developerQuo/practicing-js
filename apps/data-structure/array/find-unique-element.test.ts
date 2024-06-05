5d1877bc16fd709a363947d1349aa50b60fe2bb6
changmin <developerQuo@gmail.com>
2024-06-05 10:20:23 +0900

    test: add find unique elements interface

---
+++ apps/data-structure/array/find-unique-element.test.ts
@@ -0,0 +1,18 @@
+import { describe, expect, it } from '@jest/globals';
+import { unique } from './find-unique-element';
+
+let strings = [
+	'Hare',
+	'Krishna',
+	'Hare',
+	'Krishna',
+	'Krishna',
+	'Krishna',
+	'Hare',
+	'Hare',
+	':-O',
+];
+
+describe('중복 없는 요소 찾아내기', () => {
+	it('returns unique elements', () => {});
+});
