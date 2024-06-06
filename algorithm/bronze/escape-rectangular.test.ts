4a730dc367284a61a16e6f23d51aafe7354bc439
changmin <developerQuo@gmail.com>
2024-06-06 10:04:55 +0900

    test: add interface to excape from rectangular

---
+++ algorithm/bronze/escape-rectangular.test.ts
@@ -0,0 +1,15 @@
+import { describe, expect, it } from '@jest/globals';
+import getShortestDistance, { getAxisDistance } from './escape-rectangular';
+
+/**
+ * 직사각형에서 탈출[https://www.acmicpc.net/problem/1085]
+ *
+ * 한수는 지금 (x, y)에 있다.
+ * 직사각형은 각 변이 좌표축에 평행하고, 왼쪽 아래 꼭짓점은 (0, 0), 오른쪽 위 꼭짓점은 (w, h)에 있다.
+ * 직사각형의 경계선까지 가는 거리의 최솟값을 구하는 프로그램을 작성하시오.
+ */
+describe('직사각형에서 탈출', () => {
+	it('returns 2 distance', () => {});
+
+	it('최소 거리 구하기', () => {});
+});
---
+++ algorithm/bronze/escape-rectangular.ts
@@ -0,0 +1,15 @@
+export default function getShortestDistance(
+	x: number,
+	y: number,
+	w: number,
+	h: number
+) {
+	return 0;
+}
+
+export function getAxisDistance(
+	userLocation: number,
+	axisMaxLength: number
+): [number, number] {
+	return [0, 0];
+}
