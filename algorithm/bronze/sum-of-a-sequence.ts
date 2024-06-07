f5913fb13f5159c5faec39aace9746c948c2a8cc
changmin <developerQuo@gmail.com>
2024-06-07 10:39:31 +0900

    feat: add sumSequence function

--- algorithm/bronze/sum-of-a-sequence.ts
+++ algorithm/bronze/sum-of-a-sequence.ts
@@ -2,4 +2,29 @@ export default function calcRange(rangeX: number, rangeY: number) {
 	return 0;
 }
 
-export function sumSquence(num: number) {}
+const example = {
+	1: 1,
+	2: 2,
+	3: 1,
+};
+
+const example2 = [1, 2, 1];
+
+export function sumSquence(num: number) {
+	const sequence: number[] = [];
+	for (let i = 0; i < num; i++) {
+		const lastIndex = sequence.length - 1;
+		const lastIndexCount = sequence[lastIndex];
+		if (lastIndexCount < lastIndex + 1) {
+			sequence.splice(lastIndex, 1, lastIndexCount + 1);
+		} else {
+			sequence.push(1);
+		}
+	}
+
+	return sequence.reduce((sum, count, index) => {
+		const value = index + 1;
+		sum += value * count;
+		return sum;
+	}, 0);
+}
