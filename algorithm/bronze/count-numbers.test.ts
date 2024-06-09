18fc0ca3bdcac1edbf24e8df266d19d77c443432
changmin <developerQuo@gmail.com>
2024-06-09 19:43:49 +0900

    test: fix count-numbers interface

--- algorithm/bronze/count-numbers.test.ts
+++ algorithm/bronze/count-numbers.test.ts
@@ -1,5 +1,4 @@
 import { describe, expect, it, test } from '@jest/globals';
-import calcRange, { sumSequence } from './sum-of-a-sequence';
 
 /**
  * 숫자의 개수 [https://www.acmicpc.net/problem/2577]
@@ -8,9 +7,11 @@ import calcRange, { sumSequence } from './sum-of-a-sequence';
  *
  * 예를 들어 A = 150, B = 266, C = 427 이라면 A × B × C = 150 × 266 × 427 = 17037300 이 되고,
  * 계산한 결과 17037300 에는 0이 3번, 1이 1번, 3이 2번, 7이 2번 쓰였다.
+ *
+ * 첫째 줄에 A, 둘째 줄에 B, 셋째 줄에 C가 주어진다. A, B, C는 모두 100보다 크거나 같고, 1,000보다 작은 자연수이다.
  */
 describe('숫자의 개수', () => {
-	it('logs the count of numbers from 0 to 9', () => {});
+	it('counts digit within number', () => {});
 
-	it('multiples the three numbers', () => {});
+	it('logs the count of numbers from 0 to 9', () => {});
 });
