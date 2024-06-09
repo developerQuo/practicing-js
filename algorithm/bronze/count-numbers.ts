31b9a6b62d81601bb3f359f3dd793beee68ddf8e
changmin <developerQuo@gmail.com>
2024-06-10 00:31:56 +0900

    feat: add countNumbers function

--- algorithm/bronze/count-numbers.ts
+++ algorithm/bronze/count-numbers.ts
@@ -1,5 +1,10 @@
 export default function countNumbers(numX: number, numY: number, numZ: number) {
-	return console.log();
+	const countArray: number[] = [];
+
+	for (let digit = 0; digit < 10; digit++) {
+		countArray.push(countDigit(numX * numY * numZ, digit));
+	}
+	return countArray;
 }
 
 export function countDigit(number: number, digit: number) {
